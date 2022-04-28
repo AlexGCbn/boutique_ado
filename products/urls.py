6eda46681f2d1cd8de6e1536eabd295cb58f44b0
Alex Ciobanu <tsopi10@gmail.com>
2022-04-28 13:57:37 +0000

    Add add_product form, view and template

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<product_id>', views.product_detail, name='product_detail'),  # Product detail view
+    path('<int:product_id>/', views.product_detail, name='product_detail'),  # Product detail view
+    path('add/', views.add_product, name='add_product'),  # Add product  view
 ]
