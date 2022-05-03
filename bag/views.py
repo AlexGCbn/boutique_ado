52f6545cd19a7909214f055f49332f54b21762b8
Alex Ciobanu <tsopi10@gmail.com>
2022-05-03 13:12:45 +0000

    Final commit

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.shortcuts import (
+    render, redirect, reverse, HttpResponse, get_object_or_404
+)
 from django.contrib import messages
 from products.models import Product
 
@@ -9,6 +11,7 @@ def view_bag(request):
     """
 
     return render(request, 'bag/bag.html')
+
 
 def add_to_bag(request, item_id):
     """
@@ -46,6 +49,7 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag
     return redirect(redirect_url)
 
+
 def adjust_bag(request, item_id):
     """
     Adjust the quantity of the specified product to the specified amount
@@ -78,6 +82,7 @@ def adjust_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
+
 
 def remove_from_bag(request, item_id):
     """
