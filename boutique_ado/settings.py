9cd761a7f00f1e936d27e2a09dd1ec1cd02ff42b
Alex Ciobanu <tsopi10@gmail.com>
2022-09-29 13:14:22 +0000

    Add render allowed host settings

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -30,8 +30,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['boutique-ado-alex.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['localhost']
 
+# Add render URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
